b740373c42b24bf2892755d32c47005b7b55ad1a
Sachin Mamoru <sachinmamoru@gmail.com>
2022-09-30 23:47:34 +0530

    Added a service to resolve the users' organization hierarchy until resident organization.

--- components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/OrganizationUserResidentResolverService.java
+++ components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/OrganizationUserResidentResolverService.java
@@ -19,8 +19,10 @@
 package org.wso2.carbon.identity.organization.management.service;
 
 import org.wso2.carbon.identity.organization.management.service.exception.OrganizationManagementException;
+import org.wso2.carbon.identity.organization.management.service.model.BasicOrganization;
 import org.wso2.carbon.user.core.common.User;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -50,5 +52,15 @@ public interface OrganizationUserResidentResolverService {
      * @throws OrganizationManagementException Error occurred while resolving resident org of the user.
      */
     Optional<String> resolveResidentOrganization(String userId, String organizationId)
+            throws OrganizationManagementException;
+
+    /**
+     *
+     * @param userId The user ID.
+     * @param organizationId The given child organization in the hierarchy.
+     * @return user's organization hierarchy until resident organization
+     * @throws OrganizationManagementException Error occurred while resolving org hierarchy of the user.
+     */
+    Optional<List<BasicOrganization>> resolveResidentOrganizationHierarchy(String userId, String organizationId)
             throws OrganizationManagementException;
 }
