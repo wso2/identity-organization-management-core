ab6808f5aac7483b1ba77ca4b6efa7693ef742d5
rushannanayakkara <rushannana@gmail.com>
2023-06-06 07:18:43 +0530

    Code optimization - Remove unnecessary variable declaration

--- components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/OrganizationManagerImpl.java
+++ components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/OrganizationManagerImpl.java
@@ -336,9 +336,9 @@ public class OrganizationManagerImpl implements OrganizationManager {
                 }
             }
         }
-        int organizationDepthInHierarchy = organizationManagementDAO.getOrganizationDepthInHierarchy(organizationId);
         organizationManagementDAO.deleteOrganization(organizationId);
-        getListener().postDeleteOrganization(organizationId, organizationDepthInHierarchy);
+        getListener().postDeleteOrganization(organizationId,
+                organizationManagementDAO.getOrganizationDepthInHierarchy(organizationId));
     }
 
     @Override
