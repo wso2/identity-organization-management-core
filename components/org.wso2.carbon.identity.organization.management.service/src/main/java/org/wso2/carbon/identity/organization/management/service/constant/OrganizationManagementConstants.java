df817ffbaf3efbed6b260c3edaee7519340616ff
dewniMW <dewni.matheesha@gmail.com>
2023-10-27 06:30:35 +0530

    Add error constants related to pagination limit and offset

--- components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/constant/OrganizationManagementConstants.java
+++ components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/constant/OrganizationManagementConstants.java
@@ -354,6 +354,8 @@ public class OrganizationManagementConstants {
                 "this organization.", "The email domain provided is not associated with this organization."),
         ERROR_CODE_INVALID_DISCOVERY_ATTRIBUTE_VALUE("60092", "Invalid discovery attribute value.", "The provided " +
                 "discovery attribute(s) do not adhere to the expected format for the discovery type: %s."),
+        ERROR_CODE_INVALID_OFFSET("60093", "Invalid request.", "The provided offset value is invalid."),
+        ERROR_CODE_INVALID_LIMIT("60094", "Invalid request.", "The provided limit value is invalid."),
 
         // Server errors.
         ERROR_CODE_UNEXPECTED("65001", "Unexpected processing error",
